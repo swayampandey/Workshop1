38f1949362dbe6926a0763e6d73b4e1b459a36ee
swayampandey <swayam265@gmail.com>
2022-06-27 21:02:33 +0530

    Initial commit : UC1

---
+++ UC1.java
@@ -0,0 +1,15 @@
+
+public class UC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome Gambler game");
+
+		int EveryDayStack = 100;
+		int BetForGame = 1;
+
+		System.out.println("Every Day Stack is " + EveryDayStack);
+		System.out.println("Bet Price: " + BetForGame);
+	}
+
+}
